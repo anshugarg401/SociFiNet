6240987fd6e7f4502128576f9993b5d78a7bc2c8
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2024-05-08 13:48:48 +0530

    final project

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 // components/Home.jsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+// import classNames from 'classnames';
 import NightAndDay from '../component/nightday/NightDayToggle'
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,8 +11,71 @@ import { selectUser } from '../store/userSlice';
 import Marquee from 'react-fast-marquee';
 import {  useNavigate } from "react-router-dom";
 import ConnectMetamask from '../component/ConnectWallet';
-
-
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+const datapost = [
+  {
+    Username: "User Name",
+    Userheader: `User Headline   `,
+    usering:"https://media.istockphoto.com/id/1433039224/photo/blue-user-3d-icon-person-profile-concept-isolated-on-white-background-with-social-member.jpg?s=2048x2048&w=is&k=20&c=4kYlrBEQrLWS--wVUBYiNnMCX6psXAFLuTnARiJotiM=",
+    userpostimg:"https://media.istockphoto.com/id/1501243033/photo/chatbot-or-assistant-robot-chat-with-speech-bubble.jpg?s=2048x2048&w=is&k=20&c=Z-9vOZFkrbQRvemzBn4-AkKJCD5i0MubuRfrTSZjZ38=",
+    Domain: "Domain",
+    Content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                praesentium molestiae qui ullam at sequi, est expedita vel explicabo
+                magni quasi.`,
+  },
+  {
+    Username: "User Name",
+    Userheader: `User Headline   `,
+    usering:"https://media.istockphoto.com/id/1433039224/photo/blue-user-3d-icon-person-profile-concept-isolated-on-white-background-with-social-member.jpg?s=2048x2048&w=is&k=20&c=4kYlrBEQrLWS--wVUBYiNnMCX6psXAFLuTnARiJotiM=",
+    userpostimg:"https://media.istockphoto.com/id/1501243033/photo/chatbot-or-assistant-robot-chat-with-speech-bubble.jpg?s=2048x2048&w=is&k=20&c=Z-9vOZFkrbQRvemzBn4-AkKJCD5i0MubuRfrTSZjZ38=",
+    Domain: "Domain",
+    Content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                praesentium molestiae qui ullam at sequi, est expedita vel explicabo
+                magni quasi.`,
+  },
+  {
+    Username: "User Name",
+    Userheader: `User Headline   `,
+    usering:"https://media.istockphoto.com/id/1433039224/photo/blue-user-3d-icon-person-profile-concept-isolated-on-white-background-with-social-member.jpg?s=2048x2048&w=is&k=20&c=4kYlrBEQrLWS--wVUBYiNnMCX6psXAFLuTnARiJotiM=",
+    userpostimg:"https://media.istockphoto.com/id/1501243033/photo/chatbot-or-assistant-robot-chat-with-speech-bubble.jpg?s=2048x2048&w=is&k=20&c=Z-9vOZFkrbQRvemzBn4-AkKJCD5i0MubuRfrTSZjZ38=",
+    Domain: "Domain",
+    Content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                praesentium molestiae qui ullam at sequi, est expedita vel explicabo
+                magni quasi.`,
+  },
+  {
+    Username: "User Name",
+    Userheader: `User Headline   `,
+    usering:"https://media.istockphoto.com/id/1433039224/photo/blue-user-3d-icon-person-profile-concept-isolated-on-white-background-with-social-member.jpg?s=2048x2048&w=is&k=20&c=4kYlrBEQrLWS--wVUBYiNnMCX6psXAFLuTnARiJotiM=",
+    userpostimg:"https://media.istockphoto.com/id/1501243033/photo/chatbot-or-assistant-robot-chat-with-speech-bubble.jpg?s=2048x2048&w=is&k=20&c=Z-9vOZFkrbQRvemzBn4-AkKJCD5i0MubuRfrTSZjZ38=",
+    Domain: "Domain",
+    Content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                praesentium molestiae qui ullam at sequi, est expedita vel explicabo
+                magni quasi.`,
+  },
+  {
+    Username: "User Name",
+    Userheader: `User Headline   `,
+    usering:"https://media.istockphoto.com/id/1433039224/photo/blue-user-3d-icon-person-profile-concept-isolated-on-white-background-with-social-member.jpg?s=2048x2048&w=is&k=20&c=4kYlrBEQrLWS--wVUBYiNnMCX6psXAFLuTnARiJotiM=",
+    userpostimg:"https://media.istockphoto.com/id/1501243033/photo/chatbot-or-assistant-robot-chat-with-speech-bubble.jpg?s=2048x2048&w=is&k=20&c=Z-9vOZFkrbQRvemzBn4-AkKJCD5i0MubuRfrTSZjZ38=",
+    Domain: "Domain",
+    Content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                praesentium molestiae qui ullam at sequi, est expedita vel explicabo
+                magni quasi.`,
+  },
+  {
+    Username: "User Name",
+    Userheader: `User Headline   `,
+    usering:"https://media.istockphoto.com/id/1433039224/photo/blue-user-3d-icon-person-profile-concept-isolated-on-white-background-with-social-member.jpg?s=2048x2048&w=is&k=20&c=4kYlrBEQrLWS--wVUBYiNnMCX6psXAFLuTnARiJotiM=",
+    userpostimg:"https://media.istockphoto.com/id/1501243033/photo/chatbot-or-assistant-robot-chat-with-speech-bubble.jpg?s=2048x2048&w=is&k=20&c=Z-9vOZFkrbQRvemzBn4-AkKJCD5i0MubuRfrTSZjZ38=",
+    Domain: "Domain",
+    Content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                praesentium molestiae qui ullam at sequi, est expedita vel explicabo
+                magni quasi.`,
+  },
+];
 const { Meta } = Card;
 
 // const backgroundColor = getComputedStyle(body).backgroundColor;
@@ -28,7 +91,7 @@ const Home = () => {
   const [commentModalVisible, setCommentModalVisible] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
   const [showNext, setShowNext] = useState(true);
-
+const [Darkmode, setDarkMode] = useState(false);
   const [comment, setComment] = useState('');
   const [selectedPost, setSelectedPost] = useState(null);
   const [showPrevious, setShowPrevious] = useState(false);
@@ -38,8 +101,6 @@ const Home = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const posts = useSelector((state) => state.posts.posts);
-  
-
   const allPosts = useSelector((state) => state.posts.allPosts);
 
   const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -84,6 +145,7 @@ const Home = () => {
         return navigate('/login');
       }
 
+
       const response = await axios.post(`${BASE_URL}/api/v1/posts/like/${postId}`);
       setLikeCount(response.data.likes);
     } catch (error) {
@@ -120,6 +182,19 @@ const Home = () => {
     }
   };
 
+  const handleDarkMode = ()=>{
+    if ( !Darkmode) {
+      setDarkMode(true);
+    
+    } else {
+      setDarkMode(false);
+ 
+      
+    }
+
+    console.log(Darkmode);
+  };
+
   const [paginationList, setPaginationList] = useState([]);
 
   return (
@@ -129,11 +204,11 @@ const Home = () => {
 
     
     <div className="container mx-auto">
-   <div className="flex justify-start">
-  <NightAndDay />
+   <div className="flex justify-start"  onClick = {handleDarkMode}>
+  <NightAndDay/>
 </div>
 
-      <div className="absolute top-1 right-10 mr-4 mt-4"> 
+      <div className="absolute top-0 right-0 mr-4 mt-4"> 
         <ConnectMetamask />
       </div>
       
@@ -157,89 +232,100 @@ const Home = () => {
       </Marquee>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        
     
-          {
+          {datapost.length === 0 ? (
+        <div>
+          <img
+            src={emptysaved}
+            alt=""
+            className="object-contain h-60 xl:h-96 w-full  sm:px-10 md:px-20"
+          />
+          <div style={{ WebkitTextFillColor: "#4F51C0" }}>
+            <p className="font-semibold text-xl xl:text-3xl text-center sm:mt-6 md:mt-8 xl:mt-12">
+              No Saved Posts Yet
+            </p>
+          </div>
+        </div>
+        
+      ) : (
+        
           
-        posts?.map((post)=> (
+            datapost?.map((item) => (
               <Card
-              key={post._id}
+       
+              key={item.Username}
               hoverable
   
               style={{ width: "100%", margin: "auto" ,
-              
+            
               
               background:
                 "linear-gradient(to right, rgba(255, 255, 255, 0.2) 10%, rgba(255, 255, 255, 0.15) 60%, rgba(255, 255, 255, 0.1) 90%)",}}
-              className="mb-4 flex h-42 flex-col md:flex-row"
+                
+                 
+                className={Darkmode ? 'mb-4 flex h-42 flex-col md:flex-row text-white' : 'mb-4 flex h-42 flex-col md:flex-row'}
+
+                
+              
+                  
+            
             >
-              {/* // posts picure */}
-             
+      
+              <p key={item.Username}>
 
-
-           
-
-                  <div className=" pt-4">
-
-                  {post?.pictures.length > 0 ? (
-              <div className="md:w-80 md:border-r-2">
-                <img
-                  className="object-cover h-48 w-full rounded-md"
-                  key={post.pictures[0].public_id}
-                  src={post.pictures[0].image}
-                  alt="Post Image"
-                />
-              </div>
-            ) : (
-              <div className="md:w-80 md:border-r-2">
-                <img
-                  className="object-cover h-48 w-full rounded-md"
-                  src="/no-image.jpeg"
-                  alt="Placeholder Image"
-                />
-              </div>
-            )}
-
+                  <div className= 
+                   {classNames(
+                    " pt-4 text-inherit",
+                    {"text-black" : !Darkmode },
+                    {"text-white" : Darkmode },
+  
+                  
+                  )}
+                  >
+                    <img
+                      alt="Home"
+                      src={item.userpostimg}
+                      className=" h-56  md:h-60 w-full object-fit"
+                    />
                   </div>
 
-         
+                  <div className="mt-2">
                     <div className="mt-4 flex  items-center gap-2  text-xs ">
+                      <div className="inline-flex shrink-0 items-center gap-3 ">
+                        <a href="" className="flex-none object-cover ">
+                          <img
+                            src={item.usering}
+                            className="h-14 w-14  rounded-full"
+                            alt=""
+                          />
+                        </a>
 
+                        <div className="mt-1.5 sm:mt-0  flex-shrink">
+                          <p className=" text-lg font-semibold leading-normal ">
+                          {item.Username}
+                          </p>
 
-                  {/* // meta data content */}
-                  <Meta
-                title={
-                  <h4
-                    style={{ wordWrap: "break-word" }}
-                    className="content font-bold text-2xl h-4"
+                          <p className="font-normal text-base leading-none">
+                          {item.Userheader}
+                          </p>
+                        </div>
+                        <div className=" flex flex-row-reverse p-1 pr-5 font-light text-base  self-start flex-none w-[130px] lg:w-[190px]">
+                          <p>{item.Domain}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <dl
+                    className="mt-3 flex items-center 
+      overflow-hidden  "
                   >
-                    {post.title.substring(0, 20) +
-                      (post.content.length > 20 ? "..." : "")}
-                  </h4>
-                }
-                description={
-                  <p className="break-words content-home h-20">
-                    {post.content.substring(0, 100) +
-                      (post.content.length > 100 ? "..." : "")}
-                  </p>
-                }
-              />
-              {/* // likes and content */}
-
-<div className="mt-4 flex justify-between items-center">
-                <Link to={`/post/${post._id}`}>Read more</Link>
-                <div>
-                  <Button onClick={() => showCommentModal(post)}>
-                    Comment
-                  </Button>
-                  <Button onClick={() => handleLike(post._id)}>Like</Button>
-                  <span>{likeCount}</span>
-                </div>
-              </div>
-</div>
-            
+                    <div className="   text-base font-light px-2 ">
+                    {item.Content}
+                    </div>
+                  </dl>
+              </p>
               </Card>
-            ))}
+           ) ))}
                
 
       </div>
